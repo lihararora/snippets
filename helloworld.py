a7076af878928f34f8afe7a161e094522424c5df
Rahil Arora <Rahil@localhost.localdomain>
2014-01-11 01:45:56 -0500

    Learnt basic data types 1

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,10 @@
-ssdfsdf
+a = 2
+b = 'TIM'
+c = type(b)
+print (type(b))
+s = 'rahil'
+print (s[0:-2])
+ui = input("Enter a number:")
+uconv = int(ui)
+power = uconv**7
+print(uconv, "raised to the seventh power is:", power)
