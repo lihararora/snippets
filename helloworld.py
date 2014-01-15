9977376205e4507c7ad11a7f6152335decf79cad
Rahil <lihararora@gmail.com>
2014-01-14 20:42:24 -0500

    Printed PYTHONPATH

--- helloworld.py
+++ helloworld.py
@@ -1,12 +1,7 @@
 import sys
 alist = ["Rahil", "is", 22, "years", "old"]
-alist.remove("old")
 print(alist)
 alist.reverse()
-print(alist)
-alist.reverse()
-alist.insert(1,"Arora")
-alist.insert(5,"old")
 print(alist)
 adict = {1:"uno", 2:"dos", 3:"tres"}
 print(adict)
