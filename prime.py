c40dcfdd6bb76856940115f9386e6e612d20eb43
lihararora <lihararora@gmail.com>
2014-02-23 16:49:12 -0500

    Added prime.py

---
+++ prime.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+def is_prime(num):
+    for j in range(2,num):
+        if (num % j) == 0:
+            return False
+    return True
+
+if __name__ == "__main__":
+    low = int(input("Enter the lower bound: "))
+    high = int(input("Enter the upper bound: "))
+    for i in range(low,high):
+        if is_prime(i):
+            print(i)
+
+
