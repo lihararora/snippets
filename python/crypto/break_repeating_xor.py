5854209f09c336cd7975ca27c09b04803d4c8b67
Rahil <lihararora@gmail.com>
2015-02-27 13:38:39 -0800

    challenge6

--- python/crypto/break_repeating_xor.py
+++ python/crypto/break_repeating_xor.py
@@ -5,15 +5,74 @@
 @author: Rahil Arora
 '''
 
+import binascii
+import string
+import base64
+import single_byte_xor as SBX
+import repeating_key_xor as RKX
+
 def hamming_distance(s1, s2):
     if len(s1) != len(s2):
         raise ValueError("Undefined for sequences of unequal length")
-    print(bin(ord(s1[0])))
     d = 0
     for ch1, ch2 in zip(s1, s2):
-        d = d + bin(ord(ch1) ^ ord(ch2)).count('1')
+        d = d + bin(ch1 ^ ch2).count('1')
     return d
 
+def find_key_size(text):
+    normalized_hamming = {}
+    for size in range(2, 40):
+        b1 = text[0:size-1]
+        b2 = text[size:2*size-1]
+        b3 = text[2*size:3*size-1]
+        b4 = text[3*size:4*size-1]
+        h1 = hamming_distance(b1, b2)
+        h2 = hamming_distance(b2, b3)
+        h3 = hamming_distance(b3, b4)
+        normalized_hamming[size] = ((h1+h2+h3)/size)/3
+    return sorted(normalized_hamming.items(), key=lambda x: x[1])
+
+def is_printable(s):
+    return all(c in string.printable for c in s)
+
+def nested_list(l, s):
+    new_list = [s[i:i+l] for i in range(0, len(s), l)]
+    return new_list
+
+def find_key(cipher_bytes):
+    key = ""
+    key_size = 0
+    d = find_key_size(cipher_bytes)
+    for size, distance in d:
+        cipher_list = cipher_bytes
+        ll = nested_list(size, cipher_bytes)
+        block = bytearray()
+        for b in ll:
+            block.append(b[0])
+        key_part = SBX.detect(block)
+        x1 = ''.join(chr(key_part ^ k) for k in block)
+        if is_printable(x1):
+            key_size = size
+            key = key + chr(key_part)
+            for i in range(1, key_size):
+                block = bytearray()
+                for b in ll:
+                    try:
+                        block.append(b[i])
+                    except IndexError:
+                        break
+                key_part = SBX.detect(block)
+                x1 = ''.join(chr(key_part ^ k) for k in block)
+                if is_printable(x1):
+                    key = key + chr(key_part)
+    return key
+
+
 if __name__ == "__main__":
-    a = hamming_distance('this is a test', 'wokka wokka!!!')
-    print(a)
+    s = open('challenge6.txt').read()
+    cipher_bytes = base64.b64decode(s)
+    key = find_key(cipher_bytes)
+    print("Key = ", key)
+    hex_message = RKX.xor(cipher_bytes.decode('utf-8'), key)
+    actual_message = binascii.unhexlify(hex_message)
+    print("Message = ", actual_message)
--- python/crypto/single_byte_xor.py
+++ python/crypto/single_byte_xor.py
@@ -7,10 +7,6 @@
 
 import binascii
 
-cipher = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
-cipher_bytes = binascii.unhexlify(cipher)
-cipher_string = cipher_bytes.decode("utf-8")
-
 '''
 Returns the numner of times common characters appear in the string
 '''
@@ -19,7 +15,7 @@ def stupid_score(sentence):
     score = score + sentence.count(' ') + sentence.count('e') + sentence.count('t') + sentence.count('a') + sentence.count('o') + sentence.count('i') + sentence.count('E') + sentence.count('T') + sentence.count('A') + sentence.count('O') + sentence.count('I')
     return score
 
-if __name__ == "__main__":
+def detect(cipher_bytes):    
     high = 0
     correct_key = ' '
     for key in range(0, 256):
@@ -29,7 +25,14 @@ if __name__ == "__main__":
             high = ss
             correct_key = key
             message = s
+    return correct_key
+
+if __name__ == "__main__":
+    cipher = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
+    cipher_bytes = binascii.unhexlify(cipher)
+    cipher_string = cipher_bytes.decode("utf-8")
+    ck = detect(cipher_bytes)
     print("----------------FOUND----------------")
-    print("Key = ", correct_key)
-    #message = ''.join(chr(ord(correct_key) ^ ord(k)) for k in cipher_string)
-    print("Message = ", message.encode('utf-8'))
+    print("Key = ", ck)
+    m = ''.join(chr(ck ^ k) for k in cipher_bytes)
+    print("Message = ", m.encode('utf-8'))
